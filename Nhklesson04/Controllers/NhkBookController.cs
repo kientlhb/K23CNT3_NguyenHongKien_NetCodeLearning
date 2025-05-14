000d2a78d0e47699309623f1fedeb8e9d93c9772
kientlhb <kientlhb3@gmail.com>
2025-05-14 20:29:12 +0700

    lesson04

---
+++ Nhklesson04/Controllers/NhkBookController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nhklesson04.Models;
+using System.Collections.Generic;
+
+namespace Nhklesson04.Controllers
+{
+    public class NhkBookController : Controller
+    {
+        private readonly BookService _bookService;
+
+        public NhkBookController()
+        {
+            _bookService = new BookService();
+        }
+
+        public IActionResult NhkIndex()
+        {
+            var books = _bookService.GetAllBooks();
+            return View(books);
+        }
+    }
+
+    // Gộp BookService vào cùng file
+    public class BookService
+    {
+        private List<NhkBook> Nhkbooks = new List<NhkBook>
+        {
+            new NhkBook { Id = "1", NhkTitle = "Book Title 1", NhkDescription = "Description for book 1", NhkImage = "/image/book1.jpg", NhkPrice = "$6.99", NhkPage = "100" },
+            new NhkBook { Id = "2", NhkTitle = "Book Title 2", NhkDescription = "Description for book 2", NhkImage = "/image/book2.jpg", NhkPrice = "$7.99", NhkPage = "200" },
+            new NhkBook { Id = "3", NhkTitle = "Book Title 3", NhkDescription = "Description for book 3", NhkImage = "/image/book3.jpg", NhkPrice = "$8.99", NhkPage = "300" },
+            new NhkBook { Id = "4", NhkTitle = "Book Title 4", NhkDescription = "Description for book 4", NhkImage = "/image/book4.jpg", NhkPrice = "$9.99", NhkPage = "400" },
+            new NhkBook { Id = "5", NhkTitle = "Book Title 5", NhkDescription = "Description for book 5", NhkImage = "/image/book5.jpg", NhkPrice = "$10.99", NhkPage = "500" }
+        };
+
+        public List<NhkBook> GetAllBooks()
+        {
+            return Nhkbooks;
+        }
+    }
+}
---
+++ Nhklesson04/Models/NhkBook.cs
@@ -0,0 +1,12 @@
+﻿namespace Nhklesson04.Models
+{
+    public class NhkBook
+    {
+        public string Id { get; set; }
+        public string NhkTitle { get; set; }
+        public string NhkDescription { get; set; }
+        public string NhkImage { get; set; }
+        public string NhkPrice { get; set; }
+        public string NhkPage { get; set; }
+    }
+}
